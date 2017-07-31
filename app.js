3c139e05e2ee3ecd5c5760ffacf707009968178e
Tim Klein <tklein@outlook.com>
2017-07-31 15:36:36 -0600

    Add Token Submission Route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const query = require ('./controllers/queryController.js');
@@ -9,11 +10,17 @@ const notify = require ('./controllers/notificationController.js');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 // Create Contact Route - Find source data, create contact record in referral account, find & apply referral tag to new record
 app.post('/incoming/callrail', query.findContact, contact.createContact, notify.notifyClient);
+
+app.post('/incoming/token', function (req, res) {
+	console.log(req.body.token);
+	res.sendStatus(200);
+});
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, function() {
